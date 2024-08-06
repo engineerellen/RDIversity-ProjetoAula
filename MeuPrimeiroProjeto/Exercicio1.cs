03a935378afba4ec8e13f3f6565f1ef4d31f76d6
Ellen <36012338+engineerellen@users.noreply.github.com>
2024-08-06 17:26:01 -0300

    .

---
+++ MeuPrimeiroProjeto/Exercicio1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MeuPrimeiroProjeto
+{
+    internal class Exercicio1
+    {
+        internal string CalcularTemperatura(double temp, string unidade)
+        {
+            var temper = ConvertToCelsius(temp, unidade);
+
+            if (temper <= 0)
+                return "Congelando";
+
+            if (temper > 0 && temper < 15)
+                return "Frio";
+
+            if (temper > 15 && temper < 21)
+                return "Fresquinho";
+
+            if (temper > 21 && temper < 28)
+                return "Calor";
+
+            if (temper > 28)
+                return "Derretendo";
+
+            return "temperatura inválida!";
+        }
+
+        private double ConvertToCelsius(double temp, string unidade)
+        {
+            switch (unidade)
+            {
+                case "C":
+                    return temp;
+                case "F":
+
+                    return (temp - 32) * 5 / 9;
+
+                case "K":
+                    return (temp - 273.15);
+
+                default:
+                    return 0;
+            }
+        }
+
+    }
+}
---
+++ MeuPrimeiroProjeto/Program.cs
@@ -0,0 +1,16 @@
+﻿
+//Console.WriteLine("Minha primeira aula");
+
+
+using MeuPrimeiroProjeto;
+
+Console.WriteLine(Utility.IsNumeric(12));
+Console.WriteLine(Utility.IsNumeric(true));
+Console.WriteLine(Utility.IsNumeric('c'));
+Console.WriteLine(Utility.IsNumeric(new DateTime(2012, 1, 1)));
+Console.WriteLine(Utility.IsInteger(12.2));
+Console.WriteLine(Utility.IsInteger(123456789));
+Console.WriteLine(Utility.IsFloat(true));
+Console.WriteLine(Utility.IsFloat(12.2));
+Console.WriteLine(Utility.IsFloat(12));
+Console.WriteLine("{0} {1} {2}", 12.1, Utility.Compare(12.1, 12), 12);
